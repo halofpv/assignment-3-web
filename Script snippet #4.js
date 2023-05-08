43aba119147b7cf4f9ffe27e6d517585a0bc27c2
Rms <124281806+halofpv@users.noreply.github.com>
2023-05-08 23:13:22 +0600

    Add files via upload

---
+++ Script snippet #4.js
@@ -0,0 +1,5 @@
+const age = prompt("What is your age?");
+const month = age * 12;
+const week = age * 52;
+const day = age * 365;
+alert(`You have  ${day} days, ${week} weeks, and ${month} months left`);
